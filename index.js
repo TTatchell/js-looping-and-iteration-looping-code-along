dd9e2854711256d4a68f250e4cf13d1ae0e26702
TTatchell <tim.tatchell24@gmail.com>
2021-03-29 19:10:20 +1100

    upload

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(nameArr) {
+    let retArr = [];
+    for (let i = 0; i < nameArr.length; i++) {
+        retArr[i] = `Thank you, ${nameArr[i]}, for the wonderful surprise gift!`
+        
+    }
+    return retArr;
+}
+
+function countDown(num) {
+    for (num; num >= 0; num--) {
+        console.log(num);
+        
+    }
+}
+
+countDown(5)
